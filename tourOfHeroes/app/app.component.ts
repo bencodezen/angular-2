51d4cab6207844ad133177d4106af376c74e8083
Ben Hong <bencodezen@gmail.com>
2016-03-15 14:20:30 -0400

    tourOfHeroes: Display a list of heroes

--- tourOfHeroes/app/app.component.ts
+++ tourOfHeroes/app/app.component.ts
@@ -17,14 +17,33 @@ interface Hero {
         <input id="hero-name" [(ngModel)]="hero.name" placeholder="name">
       </div>
     </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
     `
 })
 
 export class AppComponent {
   public title = 'Tour of Heroes';
-
+  public heroes = HEROES;
   public hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" },
+];
